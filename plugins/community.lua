a580efe946facaad7b2a6844b0bbb3b7cc9b5e6b
Ying Fang <fangying712@gmail.com>
2024-02-18 17:16:22 +0800

    Use sonokai as the default colorscheme
    Signed-off-by: Ying Fang <fangying712@gmail.com>

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,6 +1,9 @@
 return {
   -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
+  { import = "astrocommunity.colorscheme.github-nvim-theme" },
+  { import = "astrocommunity.colorscheme.onedarkpro-nvim" },
+  { import = "astrocommunity.colorscheme.sonokai" },
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
