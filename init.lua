5a799016e28da732ee58ea502cdaa8a5aa75354f
Ying Fang <fangying712@gmail.com>
2024-03-16 09:54:07 +0800

    Use github_dark_default colorscheme

--- init.lua
+++ init.lua
@@ -18,7 +18,7 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "sonokai",
+  colorscheme = "github_dark_default",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
