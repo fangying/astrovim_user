1cd885f0e33820d806e636a0f319a3d1773e0e7b
Ying Fang <fangying712@gmail.com>
2024-03-16 09:52:47 +0800

    Introduce vim-highlighter plugin

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,7 @@ return {
   { import = "astrocommunity.colorscheme.github-nvim-theme" },
   { import = "astrocommunity.colorscheme.onedarkpro-nvim" },
   { import = "astrocommunity.colorscheme.sonokai" },
+  { import = "astrocommunity.color.vim-highlighter" },
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
